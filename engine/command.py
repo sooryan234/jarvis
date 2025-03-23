b52290a51544b64de46a23fb88235bba0fbc9d75
sooryan234 <sooryankrishna234@gmailexample.com>
2025-03-23 16:26:57 +0530

    chat history

--- engine/command.py
+++ engine/command.py
@@ -12,6 +12,7 @@ def speak(text):
     engine.setProperty('rate', 174)
     eel.DisplayMessage(text)
     engine.say(text)
+    eel.receiverText(text)
     engine.runAndWait()
 
 
@@ -54,8 +55,10 @@ def allCommands(message=1):
     if message ==1:
         query = takecommand()
         print(query)
+        eel.senderText(query)
     else:
         query = message
+        eel.senderText(query)
 
     try:
 
