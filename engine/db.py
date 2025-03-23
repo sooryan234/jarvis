b7c96c44d60dc8311374e1d7ccb5b4446a8b4f36
sooryan234 <sooryankrishna234@gmailexample.com>
2025-03-23 14:57:12 +0530

    whatsapp

--- engine/db.py
+++ engine/db.py
@@ -1,9 +1,10 @@
+import csv
 import sqlite3
 
 
 con = sqlite3.connect("jarvis.db")
 
-cursor = conn.cursor()
+cursor = con.cursor()
 
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 cursor.execute(query)
@@ -15,9 +16,50 @@ cursor.execute(query)
 
 
 
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
-cursor.execute(query)
+# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+# cursor.execute(query)
 
-query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
+# cursor.execute(query)
+# con.commit()
+
+
+# Create a table with the desired columns
+# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+
+
+
+# Specify the column indices you want to import (0-based index)
+# Example: Importing the 1st and 3rd columns
+# desired_columns_indices = [0, 20]
+
+# Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# # Commit changes and close connection
+# con.commit()
+# con.close()
+
+#### 4. Insert Single contacts (Optional)
+
+
+# query = "INSERT INTO contacts VALUES (null,'care giver', '7593986288','null')"
+# cursor.execute(query)
+# con.commit()
+
+
+# query = 'care giver'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(results[0][0])
+
+
+
+
--- engine/features.py
+++ engine/features.py
@@ -1,7 +1,9 @@
 import os
 import re
+from shlex import quote
 import sqlite3
 import struct
+import subprocess
 import time
 import webbrowser
 
@@ -9,11 +11,12 @@ from  playsound import playsound
 import eel
 import pvporcupine
 import pyaudio
+import pyautogui
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
 import pywhatkit as kit
 
-from engine.helper import extract_yt_term
+from engine.helper import extract_yt_term, remove_words
 
 
 
@@ -113,3 +116,65 @@ def hotword():
 
 
 
+# find contacts
+def findContact(query):
+    
+    
+    words_to_remove = [ASSISTANT_NAME, 'make', 'a', 'to', 'phone', 'call', 'send', 'message', 'wahtsapp', 'video']
+    query = remove_words(query, words_to_remove)
+
+    try:
+        query = query.strip().lower()
+        cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+        results = cursor.fetchall()
+        print(results[0][0])
+        mobile_number_str = str(results[0][0])
+        if not mobile_number_str.startswith('+91'):
+            mobile_number_str = '+91' + mobile_number_str
+
+        return mobile_number_str, query
+    except:
+        speak('not exist in contacts')
+        return 0, 0
+    
+
+# Create Whatsapp Function in features.py
+
+
+def whatsApp(mobile_no, message, flag, name):
+
+    if flag == 'message':
+        target_tab = 19
+        jarvis_message = "message send successfully to "+name
+
+    elif flag == 'call':
+        target_tab = 14
+        message = ''
+        jarvis_message = "calling to "+name
+
+    else:
+        target_tab = 13
+        message = ''
+        jarvis_message = "staring video call with "+name
+
+    # Encode the message for URL
+    encoded_message = quote(message)
+
+    # Construct the URL
+    whatsapp_url = f"whatsapp://send?phone={mobile_no}&text={encoded_message}"
+
+    # Construct the full command
+    full_command = f'start "" "{whatsapp_url}"'
+
+    # Open WhatsApp with the constructed URL using cmd.exe
+    subprocess.run(full_command, shell=True)
+    time.sleep(5)
+    subprocess.run(full_command, shell=True)
+    
+    pyautogui.hotkey('ctrl', 'f')
+
+    for i in range(1, target_tab):
+        pyautogui.hotkey('tab')
+
+    pyautogui.hotkey('enter')
+    speak(jarvis_message)
